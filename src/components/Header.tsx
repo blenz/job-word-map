1a04fe0810c5c30480f340a91f94c3aa7f586381
blenz <bbluegga@yahoo.com>
2024-06-20 18:11:56 -0700

    update header alignment

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
     return (
-        <div className="p-6">
-            <h1 className="text-xl font-semibold">Job Word Map</h1>
+        <div className="flex justify-center p-6">
+            <h1 className="text-3xl font-semibold">Job Word Map</h1>
         </div>
     );
 }
