ad44885d52c31d03b82bad4b6ea9e0579e521077
blenz <bbluegga@yahoo.com>
2024-06-26 18:48:23 -0700

    add initial terraform

--- infra/main.tf
+++ infra/main.tf
@@ -1,3 +1,7 @@
+locals {
+  project_name = "job-word-map"
+}
+
 variable "github_token" {
   type      = string
   sensitive = true
@@ -6,40 +10,53 @@ variable "github_token" {
 provider "aws" {}
 
 resource "aws_amplify_app" "this" {
-  name                 = "job-word-map"
-  platform             = "WEB"
+  name                 = local.project_name
+  platform             = "WEB_COMPUTE"
   repository           = "https://github.com/blenz/job-word-map"
   access_token         = var.github_token
   iam_service_role_arn = aws_iam_role.this.arn
 
-  #   environment_variables = {
-  #     NEXT_PUBLIC_API_URL = "https://api.example.com"
-  #   }
+  environment_variables = {
+    "_" = "",
+  }
 
-  build_spec = yamlencode({
-    version = 1.0
-    frontend = {
-      phases = {
-        preBuild = {
-          commands = ["npm install"]
-        }
-        build = {
-          commands = ["npm run build"]
-        }
-        artifacts = {
-          baseDirectory = ".next"
-          files         = ["**/*"]
-        }
-        cache = {
-          paths = ["node_modules/**/*"]
-        }
-      }
-    }
-  })
+  build_spec = <<-EOT
+    version: 1
+    frontend:
+      phases:
+        preBuild:
+          commands:
+            - npm ci
+        build:
+          commands:
+            - npm run build
+      artifacts:
+        baseDirectory: .next
+        files:
+          - '**/*'
+      cache:
+        paths:
+          - .npm/**/*
+          - .next/cache/**/*
+  EOT
 }
 
+resource "aws_amplify_branch" "this" {
+  app_id            = aws_amplify_app.this.id
+  branch_name       = "main"
+  enable_auto_build = false
+
+  framework = "Next.js - SSR"
+  stage     = "PRODUCTION"
+}
+
+# resource "aws_amplify_webhook" "this" {
+#   app_id      = aws_amplify_app.this.id
+#   branch_name = aws_amplify_branch.this.branch_name
+# }
+
 resource "aws_iam_role" "this" {
-  name = "amplify"
+  name = "${local.project_name}-amplify"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
@@ -53,6 +70,6 @@ resource "aws_iam_role" "this" {
   })
 
   managed_policy_arns = [
-    "arn:aws:iam::aws:policy/AdministratorAccess"
+    "arn:aws:iam::aws:policy/service-role/AmplifyBackendDeployFullAccess"
   ]
 }
